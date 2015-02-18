2d7829df3317e0fe222b7440170ee6a3d2b6fb52
Norrathep Rattanavipanon <superoak90@gmail.com>
2015-02-17 20:40:15 -0800

    properly ignore map.txt and print progress & running time

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,22 @@ import java.util.List;
 public class Main {
 	
 	public static final String ROOT_FOLDER = "./data/crawl/root/";
-	public static final String MAP_PATH = "./data/crawl/root/map.txt";
+	public static final String MAP_FILENAME = "map.txt";
+	public static final String MAP_PATH = ROOT_FOLDER+MAP_FILENAME;
 	
 	public static void main(String args[]) throws IOException {
 		File[] listOfFiles = new File(ROOT_FOLDER).listFiles();
 		
 		Index index = new Index();
+		final long startTime = System.currentTimeMillis();
 		
 		for (File file : listOfFiles) {
 			// all index processing should be in here
 			String docId = file.getName();
 			
 			// check if its valid file
-			if (file.isFile() && !docId.equals(MAP_PATH)) {
+			if (file.isFile() && !docId.equals(MAP_FILENAME)) {
+				System.out.println("Processing file: "+docId);
 			    List<String> tokens = Token.tokenizeFile(file.getAbsolutePath());
 			    
 			    // ----- position index processing starts ------
@@ -36,5 +39,9 @@ public class Main {
 		PrintWriter out = new PrintWriter("out.txt");
 		out.print(index.toString());
 		out.close();
+		
+		final long endTime = System.currentTimeMillis();
+		
+		System.out.println("Total execution time: " + (endTime - startTime) );
 	}
 }
