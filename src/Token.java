e4abc422a0dd0c7e5d7b4ec0e92f8a60b8c6dea5
Norrathep Rattanavipanon <superoak90@gmail.com>
2015-02-24 12:12:49 -0800

    add tokenize sentences

--- src/Token.java
+++ src/Token.java
@@ -8,6 +8,23 @@ import java.util.Scanner;
 
 public class Token {
 	
+	public static List<String> tokenizeQuery(String query) {
+		List<String> out = new ArrayList<String>();
+		Scanner fileScanner;
+		fileScanner = new Scanner(query);
+   	 	String word;
+		while (fileScanner.hasNext()) {
+    		 word = fileScanner.next();
+        	 String[] words = word.split("[^a-zA-Z0-9]");
+        	 for(int i=0; i<words.length; i++) {
+        		 if(words[i].isEmpty()) continue;
+        		 out.add(tokenize(words[i]));
+        	 }
+		}
+		fileScanner.close();
+		return out;
+	}
+	
 	public static List<String> tokenizeFile(String fileName) throws FileNotFoundException {
 		File textFile = new File(fileName);
 		List<String> out = new ArrayList<String>();
