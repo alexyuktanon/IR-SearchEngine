1f3adefd1c3a5e957552b6f5ff25ca67749218cc
Akrapon Alex Yuktanon <alex_y26@hotmail.com>
2015-02-28 00:37:25 -0800

    Code cleanup and refactor code

--- src/Main2.java
+++ src/Main2.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
